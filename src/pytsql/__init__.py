93fe62e9a03114e2f70305d5174eb9d5ff7a879b
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2022-02-15 22:59:58 +0100

    cleanup

--- src/pytsql/__init__.py
+++ src/pytsql/__init__.py
@@ -1,11 +1,9 @@
-"""Execution of SQL scripts for MSSQL"""
-# import pkg_resources
+"""`Pytsql` allows to run mssql sripts, typically run via GUIs, via CLI."""
+
 
 from .tsql import execute, executes
 
-# try:
-    # __version__ = pkg_resources.get_distribution(__name__).version
-# except Exception:
+
 __version__ = "0.1"
 
 __all__ = ["execute", "executes"]
