182cf1dca39c26f3abc730042ef82a09dbdc82ed
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2022-02-15 14:10:44 +0100

    packaging

--- src/pytsql/__init__.py
+++ src/pytsql/__init__.py
@@ -1,10 +1,11 @@
-import pkg_resources
+"""Execution of SQL scripts for MSSQL"""
+# import pkg_resources
 
 from .tsql import execute, executes
 
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except Exception:
-    __version__ = "unknown"
+# try:
+    # __version__ = pkg_resources.get_distribution(__name__).version
+# except Exception:
+__version__ = "0.1"
 
 __all__ = ["execute", "executes"]
