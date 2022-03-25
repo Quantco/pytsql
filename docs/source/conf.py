4098926f033067d5f749e22549b1086b55d479ec
kklein <kleinkevin.lu@gmail.com>
2022-03-25 14:06:22 +0100

    Add individual class to automock.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-autodoc_mock_imports = ["antlr4", "sqlalchemy"]
+autodoc_mock_imports = ["antlr4", "sqlalchemy", "antlr4.Lexer"]
 
 print(f"READ THE DOCS DEBUG {os.getcwd()}")
 sys.path.insert(0, os.path.abspath("../../src"))
