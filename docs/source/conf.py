eff0003dd5b9537893a13aa977aa6336209bae24
kklein <kleinkevin.lu@gmail.com>
2022-03-25 11:39:25 +0100

    Mock import of sqlalchemy.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-autodoc_mock_imports = ["antlr4"]
+autodoc_mock_imports = ["antlr4", "sqlalchemy"]
 
 print(f"READ THE DOCS DEBUG {os.getcwd()}")
 sys.path.insert(0, os.path.abspath("../../src"))
