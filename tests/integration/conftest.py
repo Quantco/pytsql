83477170c3e763cfb398b42c1c9ab9e7ce2cfc80
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2022-04-11 13:21:28 +0200

    adding integration test suite execution in CI (#8)

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -9,21 +9,20 @@ TEST_DB_NAME = "tempdb"
 
 
 def get_engine(backend) -> sa.engine.Engine:
-    address = os.environ.get("DB_ADDR", "localhost:1433")
-    auth = "sa:QuantCo%40MSSQL@" if backend != "mssql-windows-ci" else ""
-    connection_string = f"mssql+pyodbc://{auth}{address}/{TEST_DB_NAME}"
+    address = os.environ.get("DB_ADDR", "localhost")
+    # auth = "sa:QuantCo%40MSSQL@" if backend != "mssql-windows-ci" else ""
+    connection_string = f"mssql+pyodbc://sa:pytsql-Test-123@{address}:1433/{TEST_DB_NAME}"
     if backend == "mssql-freetds":
         connection_string += "?driver=libtdsodbc.so&tds_version=7.4"
     else:
         msodbc_driver_name = urllib.parse.quote_plus("ODBC Driver 17 for SQL Server")
         connection_string += f"?driver={msodbc_driver_name}"
-    return sa.create_engine(connection_string, connect_args={"autocommit": True})
+    return sa.create_engine(connection_string, connect_args={"autocommit": True, "timeout": 10})
 
 
 @pytest.fixture(scope="function")
 def engine(backend):
-    engine = get_engine(backend)
-    return engine
+    yield get_engine(backend)
 
 
 def pytest_addoption(parser):
@@ -39,10 +38,3 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize(
             "backend", [metafunc.config.getoption("backend")], scope="module"
         )
-
-
-@pytest.fixture(scope="session", autouse=True)
-def setup_odbcsysini():
-    os.environ["ODBCSYSINI"] = str(
-        Path(os.environ["CONDA_PREFIX"]) / "etc" / "msodbcsql17"
-    )
