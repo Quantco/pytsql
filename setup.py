2c1a9ddfa4424a2ec1e1e07d8fd3c7be9a271760
Rokas Urbonas <rokas.urbonas@quantco.com>
2022-07-27 18:29:03 +0200

    Add ANTLR C++ and speedy tool code in grammar and update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+import fnmatch
+import os
+import platform
 from time import time
 
-from setuptools import setup
+import setuptools
 from setuptools_scm.version import ScmVersion
+
+target = platform.system().lower()
+PLATFORMS = {"windows", "linux", "darwin", "cygwin"}
+for known in PLATFORMS:
+    if target.startswith(known):
+        target = known
 
 
 def get_dev_timestamp(version: ScmVersion) -> str:
@@ -17,9 +26,39 @@ def get_dev_timestamp(version: ScmVersion) -> str:
     return version.format_with(f"{{tag}}.dev{int(time())}")
 
 
-setup(
-    use_scm_version={
-        "version_scheme": get_dev_timestamp,
-        "local_scheme": "dirty-tag",
-    },
-)
+def run_setup():
+    extra_compile_args = {
+        "windows": ["/DANTLR4CPP_STATIC", "/Zc:__cplusplus"],
+        "linux": ["-std=c++11"],
+        "darwin": ["-std=c++11"],
+        "cygwin": ["-std=c++11"],
+    }
+
+    parser_ext = setuptools.Extension(
+        name="pytsql.grammar.sa_tsql_cpp_parser",
+        include_dirs=["src/pytsql/grammar/cpp_src/antlr4-cpp-runtime"],
+        sources=get_files("src/pytsql/grammar/cpp_src", "*.cpp"),
+        depends=get_files("src/pytsql/grammar/cpp_src", "*.h"),
+        extra_compile_args=extra_compile_args.get(target, []),
+    )
+
+    setuptools.setup(
+        ext_modules=[parser_ext],
+        use_scm_version={
+            "version_scheme": get_dev_timestamp,
+            "local_scheme": "dirty-tag",
+        },
+    )
+
+
+def get_files(path, pattern):
+    """Return all filenames matching the path and the pattern"""
+
+    matches = []
+    for root, _, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, pattern):
+            matches.append(os.path.join(root, filename))
+    return matches
+
+
+run_setup()
---
+++ tests/unit/test_py_vs_cpp.py
@@ -0,0 +1,130 @@
+import inspect
+import re
+from unittest import mock
+
+import pytest
+from antlr4 import InputStream, ParserRuleContext
+from antlr4.Token import CommonToken
+from antlr4.tree.Tree import TerminalNodeImpl
+
+from pytsql.grammar import sa_tsql
+from pytsql.tsql import _split
+
+
+@pytest.fixture
+def seed():
+    return """
+DECLARE @timestamp_started  DATETIME  = CURRENT_TIMESTAMP;
+-- step 0: deduce associated nr's
+DROP TABLE IF EXISTS #temporary;
+CREATE TABLE #temporary (
+    nr      VARCHAR(17)   NOT NULL
+  , short_nr         SMALLINT NOT NULL
+  , min_nr_date     DATE     NOT NULL
+  , iter        TINYINT  NOT NULL -- iterator
+  , PRIMARY KEY (nr, min_date, iter)
+);
+PRINT(CAST(GETDATE() AS VARCHAR) + ': INSERT INTO #temporary')
+INSERT INTO #temporary WITH(TABLOCK)
+SELECT CONCAT('0', nr) nr
+     , short_nr
+     , MIN(nr_date) min_nr_date
+     , ROW_NUMBER() OVER(PARTITION BY nr, MIN(nr_date) ORDER BY short_nr DESC) -- very few instances where this matters
+FROM otherdb.dbo.othertable WITH(NOLOCK)
+WHERE kind = 1
+GROUP BY nr, short_nr;"""
+
+
+def get_rule_labels(context_cls):
+    init_func = context_cls.__init__
+
+    # Detect any context/token labels from the init function's assignments
+    # Conveniently, the type name is provided as a comment
+    lines, _ = inspect.getsourcelines(init_func)
+    labels = []
+    for line in lines:
+        m = re.match(r"self\.(\w+)\s*=\s*None", line.strip())
+        if m:
+            labels.append(m.group(1))
+
+    return labels
+
+
+def compare_common_token(py_token, cpp_token):
+    assert type(py_token) == type(cpp_token)
+    if py_token is None:
+        return
+    assert py_token.type == cpp_token.type
+    assert py_token.channel == cpp_token.channel
+    assert py_token.start == cpp_token.start
+    assert py_token.stop == cpp_token.stop
+    assert py_token.tokenIndex == cpp_token.tokenIndex
+    assert py_token.line == cpp_token.line
+    assert py_token.column == cpp_token.column
+    assert py_token.text == cpp_token.text
+    assert py_token.getInputStream() is cpp_token.getInputStream()
+
+
+def compare_terminal_node(py_terminal_node, cpp_terminal_node):
+    assert type(py_terminal_node) is type(cpp_terminal_node)
+    assert type(py_terminal_node.symbol) is type(cpp_terminal_node.symbol)  # noqa: E721
+
+    compare_common_token(py_terminal_node.symbol, cpp_terminal_node.symbol)
+
+
+def validate_context(py_context, cpp_context):
+    assert type(py_context) == type(cpp_context)
+    assert len(py_context.children) == len(cpp_context.children)
+
+    # Validate children
+    for i in range(len(py_context.children)):
+        if isinstance(py_context.children[i], TerminalNodeImpl):
+            compare_terminal_node(py_context.children[i], cpp_context.children[i])
+        elif isinstance(py_context.children[i], ParserRuleContext):
+            validate_context(py_context.children[i], cpp_context.children[i])
+        else:
+            raise RuntimeError
+        assert py_context.children[i].parentCtx is py_context
+        assert cpp_context.children[i].parentCtx is cpp_context
+
+    # Validate labels
+    for label in get_rule_labels(py_context):
+        py_label = getattr(py_context, label)
+        cpp_label = getattr(cpp_context, label)
+        assert type(py_label) == type(cpp_label)
+        if isinstance(py_label, CommonToken):
+            compare_common_token(py_label, cpp_label)
+        elif isinstance(py_label, ParserRuleContext):
+            validate_context(py_label, cpp_label)
+
+
+def test_compare_py_and_cpp_parse_trees(seed):
+    """Compares the trees produced by Python and C++ version of the parser
+
+    Based on validation code included in `speedy_antlr_tool`
+    https://github.com/amykyta3/speedy-antlr-tool/blob/master/speedy_antlr_tool/validate.py
+
+    Some assertions have been removed to account for (closed but still present) issue
+    https://github.com/amykyta3/speedy-antlr-tool/issues/3
+    """
+
+    stream = InputStream(data=seed)
+
+    py_context = sa_tsql._py_parse(stream, "tsql_file")
+    cpp_context = sa_tsql._cpp_parse(stream, "tsql_file")
+
+    validate_context(py_context, cpp_context)
+
+
+@mock.patch("pytsql.grammar.sa_tsql._cpp_parse", wraps=sa_tsql._cpp_parse)
+@mock.patch("pytsql.grammar.sa_tsql._py_parse", wraps=sa_tsql._py_parse)
+def test_compare_py_and_cpp__split(py_parse_mock, cpp_parse_mock, seed):
+    sa_tsql.USE_CPP_IMPLEMENTATION = False
+    py_result = _split(seed)
+    py_parse_mock.assert_called_once()
+
+    sa_tsql.USE_CPP_IMPLEMENTATION = True
+    cpp_result = _split(seed)
+    cpp_parse_mock.assert_called_once()
+
+    assert py_result == cpp_result
