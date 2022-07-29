a91da256d14875094385a6bc7bbf3f8c51c2371c
Rokas Urbonas <rokas.urbonas@quantco.com>
2022-07-29 14:43:06 +0200

    Update custom version scheme naming and add a dosctring to it

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ from setuptools import setup
 from setuptools_scm.version import ScmVersion
 
 
-def dev_and_timestamp_scheme(version: ScmVersion) -> str:
+def get_dev_timestamp(version: ScmVersion) -> str:
+    """Returns a new distribution version string.
+
+    Returns the version found in the git tag if currently checked out commit has a tag.
+    Otherwise, returns the version found in the latest previous git tag, appended with `dev` + current timestamp
+    """
+
     if version.exact:
         return version.format_with("{tag}")
     return version.format_with(f"{{tag}}.dev{int(time())}")
@@ -12,7 +18,7 @@ def dev_and_timestamp_scheme(version: ScmVersion) -> str:
 
 setup(
     use_scm_version={
-        "version_scheme": dev_and_timestamp_scheme,
+        "version_scheme": get_dev_timestamp,
         "local_scheme": "dirty-tag",
     },
 )
