a84e652b088d152f79d3b5ceada9ca4188e4fc28
RokasUrbonasQC <108399770+RokasUrbonasQC@users.noreply.github.com>
2022-08-01 21:39:14 +0200

    Change docstring mood to imperative
    Co-authored-by: Kevin Klein <7267523+kklein@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools_scm.version import ScmVersion
 
 
 def get_dev_timestamp(version: ScmVersion) -> str:
-    """Returns a new distribution version string.
+    """Return a new distribution version string.
 
     Returns the version found in the git tag if currently checked out commit has a tag.
     Otherwise, returns the version found in the latest previous git tag, appended with `dev` + current timestamp
