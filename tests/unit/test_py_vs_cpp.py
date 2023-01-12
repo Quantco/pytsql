242e78e8fb244c161e9bc29686c926c9daeafc12
Simeon Stoykov <simeon.stoykov@quantco.com>
2023-01-12 12:47:29 +0200

    Ran PCH

--- tests/unit/test_py_vs_cpp.py
+++ tests/unit/test_py_vs_cpp.py
@@ -8,7 +8,6 @@ from antlr4 import InputStream, ParserRuleContext
 from antlr4.Token import CommonToken
 from antlr4.tree.Tree import TerminalNodeImpl
 
-from pytsql import tsql
 from pytsql.grammar import sa_tsql
 from pytsql.tsql import _split
 
