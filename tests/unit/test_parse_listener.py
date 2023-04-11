27fc5994fadb8f19b24d038fb7dd8e24cf001305
Sven Giegerich <sven.giegerich@quantco.com>
2023-04-11 09:41:57 +0200

    Add test

---
+++ tests/unit/test_parse_listener.py
@@ -0,0 +1,23 @@
+import antlr4.tree.Tree
+from antlr4 import InputStream
+
+from pytsql.grammar import TSqlParserListener, parse
+
+
+class TestTSqlParserListener(TSqlParserListener.ParseTreeListener):
+    def __init__(self):
+        self.called = False
+
+    def enterTsql_file(self, ctx):
+        self.called = True
+
+
+def test_parser_listener_called_at_enter_tsql_file():
+    seeed = "SELECT * FROM tbl"
+
+    tree = parse(InputStream(data=seeed), "tsql_file")
+    walker = antlr4.ParseTreeWalker()
+    test_listener = TestTSqlParserListener()
+    walker.walk(test_listener, tree)
+
+    assert test_listener.called
