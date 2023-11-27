0b1e43f63b3de4cc5fa6d420cb357b291624f03f
Simeon Stoykov <simeon.stoykov@quantco.com>
2023-11-27 11:29:56 +0100

    Have some options

--- tests/integration/test_parseable.py
+++ tests/integration/test_parseable.py
@@ -1,9 +1,23 @@
+import pytest
 from antlr4 import InputStream
 
 from pytsql.grammar import parse
 from pytsql.tsql import _RaisingErrorListener
 
 
-def test_bulk_insert():
-    stmt = "BULK INSERT tabname FROM '/path/to/file'"
-    tree = parse(InputStream(data=stmt), "tsql_file", _RaisingErrorListener())
+@pytest.mark.parametrize(
+    "statement",
+    [
+        "BULK INSERT tabname FROM '/path/to/file'",
+        # TODO in this PR: add the rest
+        (
+            """BULK INSERT tabname FROM '/path/to/file' WITH
+        ( BATCHSIZE = 1000
+         ,TABLOCK
+         ,FIELDTERMINATOR = ';',
+        )"""
+        ),
+    ],
+)
+def test_bulk_insert_doesnt_fail(statement: str):
+    parse(InputStream(data=statement), "tsql_file", _RaisingErrorListener())
