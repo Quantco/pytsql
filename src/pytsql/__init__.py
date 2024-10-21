67af8c94aa94a713a87bde366437552404da6da4
Pavel Zwerschke <pavelzw@gmail.com>
2024-10-21 14:31:33 +0200

    Use importlib.metadata instead of pkg_resources

--- src/pytsql/__init__.py
+++ src/pytsql/__init__.py
@@ -1,12 +1,15 @@
 """`Pytsql` allows to run mssql sripts, typically run via GUIs, via CLI."""
 
-import pkg_resources
+import importlib.metadata
+import warnings
 
 from .tsql import execute, executes
 
 try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except Exception:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError as e:  # pragma: no cover
+    warnings.warn(f"Could not determine version of {__name__}\n{e!s}", stacklevel=2)
     __version__ = "unknown"
+
 
 __all__ = ["execute", "executes"]
