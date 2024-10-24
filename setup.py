842cf90ba5ab3c7ff50049af38a558a2ebc96c4e
kklein <kevin.klein@quantco.com>
2024-10-24 16:03:01 +0200

    Adapt scm build.

--- setup.py
+++ setup.py
@@ -3,20 +3,6 @@ import platform
 from time import time
 
 import setuptools
-from setuptools_scm.version import ScmVersion
-
-
-def get_dev_timestamp(version: ScmVersion) -> str:
-    """Return a new distribution version string.
-
-    Returns the version found in the git tag if currently checked out commit has a tag.
-    Otherwise, returns the version found in the most recent git tag, appended with
-    `dev` and the current timestamp in seconds.
-    """
-
-    if version.exact:
-        return version.format_with("{tag}")
-    return version.format_with(f"{{tag}}.dev{int(time())}")
 
 
 def get_platform() -> str:
@@ -61,10 +47,6 @@ def create_extension() -> setuptools.Extension:
 def run_setup():
     setuptools.setup(
         ext_modules=[create_extension()],
-        use_scm_version={
-            "version_scheme": get_dev_timestamp,
-            "local_scheme": "dirty-tag",
-        },
     )
 
 
