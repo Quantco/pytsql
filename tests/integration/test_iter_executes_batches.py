49d91ea3dd689189bb720532616975032538c9a3
IvanDimitrovQC <ivan.dimitrov@quantco.com>
2025-09-22 13:00:13 +0300

    logging instead of print

--- tests/integration/test_iter_executes_batches.py
+++ tests/integration/test_iter_executes_batches.py
@@ -1,20 +1,21 @@
 from pytsql.tsql import iter_executes_batches
+import logging
 
 
 def test_executes_batches(engine):
     seed = """
     USE [tempdb]
     GO
-    DROP TABLE IF EXISTS [test_table]
-    CREATE TABLE [test_table] (
+    DROP TABLE IF EXISTS [test_table_batches]
+    CREATE TABLE [test_table_batches] (
         col VARCHAR(3)
     )
     GO
-    INSERT INTO [test_table] (col)
+    INSERT INTO [test_table_batches] (col)
     VALUES ('A'), ('AB'), ('ABC')
     PRINT('Affected ' + CAST(@@ROWCOUNT AS VARCHAR) + ' rows')
     """
 
     for sql, run in iter_executes_batches(seed, engine, None):
-        print(sql)
+        logging.info(sql)
         run()
